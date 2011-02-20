137186d85f07322a006de643d38fe6f960e400ab
Heikki Kallasjoki <fis@zem.fi>
2011-02-20 23:42:50 +0200

    Propagate bugfix from gear to crank

--- cranklance.c
+++ cranklance.c
@@ -1,4 +1,38 @@
-/* cranklance bfjoust interpreter; based on chainlance */
+/*
+ * cranklance bfjoust interpreter; based on chainlance.
+ *
+ * Copyright 2011 Heikki Kallasjoki. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY HEIKKI KALLASJOKI ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL HEIKKI KALLASJOKI OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and
+ * documentation are those of the authors and should not be
+ * interpreted as representing official policies, either expressed or
+ * implied, of Heikki Kallasjoki.
+ */
 
 #include <fcntl.h>
 #include <setjmp.h>
@@ -219,7 +253,7 @@ static void run(struct oplist *opsA, struct oplist *opsB)
 	static unsigned char tapemax[2][MAXTAPE];
 
 	int ipA = 0, ipB = 0;
-	unsigned char *ptrA = 0, *ptrB = 0;
+	unsigned char *ptrA = 0, *ptrB = 0, bcache = 0;
 	int repA = 0, repB = 0, *repSA = 0, *repSB = 0;
 	int deathsA = 0, deathsB = 0;
 	int cycles = 0;
@@ -235,7 +269,7 @@ static void run(struct oplist *opsA, struct oplist *opsB)
 	  \
 		memset(tape, 0, tapesize); \
 		ptrA = &tape[0], ptrB = &tape[tapesize-1]; \
-		*ptrA = 128, *ptrB = 128; \
+		*ptrA = 128, *ptrB = 128; bcache = 128; \
 	  \
 		repSA = repStackA, repSB = repStackB; \
 		deathsA = 0, deathsB = 0; \
@@ -324,6 +358,7 @@ nextcycle:
 	if (!cycles)
 		goto *ret;
 
+	bcache = *ptrB;
 	goto *opcA[ipA];
 
 fallA:
@@ -384,13 +419,13 @@ op_loop1A:
 	if (!*ptrA) ipA = oplA[ipA].match;
 	NEXTA;
 op_loop1B:
-	if (!*ptrB) ipB = oplB[ipB].match;
+	if (!bcache) ipB = oplB[ipB].match;
 	NEXTB;
 op_loop2A:
 	if (*ptrA) ipA = oplA[ipA].match;
 	NEXTA;
 op_loop2B:
-	if (*ptrB) ipB = oplB[ipB].match;
+	if (bcache) ipB = oplB[ipB].match;
 	NEXTB;
 
 	/* simple (..) repeats with no corresponding {..} block */
