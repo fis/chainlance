048327305f35562eb48381e8601fcdd7303000da
Heikki Kallasjoki <fis@zem.fi>
2014-09-17 23:11:51 +0300

    Add initial Python glue for a gearlanced hill

---
+++ zhill/hill.py
@@ -0,0 +1,342 @@
+import os
+import subprocess as sp
+from configparser import ConfigParser
+from glob import glob
+from os.path import join
+
+from zhill.gear import Gear
+from zhill.score import scorings
+
+# Hill combat logic:
+#
+# Hill consists of N programs.  When a new challenger is submitted,
+# the currently worst-ranking program is replaced by the challenger.
+# If the program name exactly matches an existing program, that one is
+# replaced instead.  Then the duel points and rankings are recomputed.
+
+# self._dir: hill repository directory
+# self._progs: dict name -> progobj
+# self._hill: list name -- in order of progobj['id']
+# self._results: dict name -> dict name -> list int -- only for (A, B) pairs where A['id'] < B['id']
+# self._ranking: list name -- in reverse order of score
+# self._gear: gearlanced instance running the hill
+# self._max_score: maximum theoretical score under current scoring method
+# self._tapecount: total number of tape configurations on this hill
+# self._scoring: current scoring mechanism
+#
+# progobj:
+#   'id': integer index in _hill and in gear
+#   'name': program canonical name
+#   'points': sum of duel points for scoring
+#   'score': current score under the scoring mechanic
+#   'rank': current index in _ranking
+#   'file': stored source file, or None if a pseudo-program
+
+class Hill:
+    """General logic for maintaining a (version-controlled) BF Joust hill."""
+
+    def __init__(self, hilldir):
+        self._dir = hilldir
+        os.chdir(hilldir)
+
+        # verify that hill directory is a Git repo
+
+        sp.check_call(('git', 'rev-parse', 'HEAD'), stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+
+        # load configuration
+
+        cfg = ConfigParser()
+        cfg.read('hill.cfg')
+        if 'hill' not in cfg:
+            raise Exception('no hill.cfg or [hill] section found')
+        hillcfg = cfg['hill']
+
+        nprogs = int(hillcfg['size'])
+        self._scoring = scorings[hillcfg['scoring']]
+        self._gear = Gear(nprogs, hillcfg['gearlanced'])
+        self._tapecount = int(hillcfg['tapecount'])
+        self._tiebreak = hillcfg['tiebreak']
+
+        self._cfg = cfg # for supplementary configuration
+
+        # construct the initial hill with initial results
+
+        self._progs = {}
+        self._hill = []
+        self._results = {}
+
+        progfiles = sorted(glob('*.bfjoust'))
+
+        for idp in range(nprogs):
+            # prepare program
+
+            p = { 'id': idp }
+            if idp < len(progfiles):
+                p['name'] = progfiles[idp][:-8]
+                p['file'] = progfiles[idp]
+                with open(p['file'], 'rb') as f:
+                    code = f.read()
+            else:
+                p['name'] = 'dummy-{0}'.format(idp)
+                code = b'<'
+
+            prog = p['name']
+
+            # insert into data structures
+
+            self._progs[prog] = p
+            self._hill.append(prog)
+            self._results[prog] = {}
+
+            # test against previous programs to update results
+
+            if idp > 0:
+                err, result = self._gear.test(code)
+                if err is not None: raise Exception(err)
+                for idA in range(idp):
+                    progA = self._hill[idA]
+                    self._results[progA][prog] = [-p for p in result[idA]]
+
+            # put into gearlanced
+
+            err = self._gear.set(idp, code)
+            if err is not None: raise Exception(err)
+
+        # compute initial scores
+
+        self._max_score = self._scoring['max'](self)
+        self._rescore()
+
+        pass
+
+    def cfg(self):
+        return self._cfg
+
+    def tapecount(self):
+        return self._tapecount
+
+    # implement a set-like abstraction for the program names
+
+    def __len__(self):
+        return len(self._hill)
+
+    def __iter__(self):
+        return iter(self._hill)
+
+    def __getitem__(self, key):
+        return self._hill[key]
+
+    def __contains__(self, prog):
+        return prog in self._progs
+
+    def progs(self):
+        return self._hill
+
+    def pairs(self):
+        n = len(self._hill)
+        for idA in range(n-1):
+            for idB in range(idA+1, n):
+                yield self._hill[idA], self._hill[idB]
+
+    def idpairs(self):
+        n = len(self._hill)
+        for idA in range(n-1):
+            for idB in range(idA+1, n):
+                yield idA, self._hill[idA], idB, self._hill[idB]
+
+    def id(self, prog):
+        return self._progs[prog]['id']
+
+    # accessors for raw results and duel points, as well as current ranking
+
+    def results(self, progA, progB):
+        progA = self._progs[progA]
+        progB = self._progs[progB]
+
+        if progA['id'] == progB['id']:
+            return [0] * (2*self._tapecount)
+
+        scale = 1
+        if progA['id'] > progB['id']:
+            scale, progA, progB = -1, progB, progA
+
+        return [scale*p for p in self._results[progA['name']][progB['name']]]
+
+    def points(self, prog):
+        return self._progs[prog]['points']
+
+    def ranking(self):
+        for prog in self._ranking:
+            p = self._progs[prog]
+            yield prog, p['points'], p['score']
+
+    def max_score(self):
+        return self._max_score
+
+    # hill program file management
+
+    def _save(self, replaced, newprog, code):
+        newfile = newprog + '.bfjoust'
+
+        if replaced['file'] is not None and replaced['file'] != newfile:
+            git('rm', '-q', '--', replaced['file'])
+            replaced['file'] = None
+
+        with open(newfile, 'wb') as sf:
+            sf.write(code)
+        git('add', '--', newfile)
+
+        if replaced['name'] == newprog:
+            commitmsg = 'Updating {0}'.format(newprog)
+        else:
+            commitmsg = 'Replacing {0} by {1}'.format(replaced['name'], newprog)
+
+        git('commit', '-q', '-m', commitmsg)
+
+        return newfile
+
+    # a new challenger appears (or an old one is changed)
+
+    def challenge(self, newprog, code):
+        # select a program to replace
+
+        replaced = self._progs.get(newprog, self._progs[self._ranking[-1]])
+        newid = replaced['id']
+
+        # test for parse errors, compute scores, replace old program
+
+        err, result = self._gear.test(code)
+        if err is not None:
+            return '{0} failed: {1}'.format(newprog, err)
+
+        err = self._gear.set(newid, code)
+        if err is not None:
+            return '{0} impossibly failed: {1}'.format(newprog, err)
+
+        # replace old program with new in hill data structures
+
+        newp = { 'id': newid,
+                 'name': newprog }
+
+        del self._progs[replaced['name']]
+        self._progs[newprog] = newp
+
+        self._hill[newid] = newprog
+
+        # remove old results
+
+        del self._results[replaced['name']]
+        for progA in self._hill[:newid]:
+            del self._results[progA][replaced['name']]
+
+        # insert in new results
+
+        self._results[newprog] = {}
+
+        for oppid, oppres in enumerate(result):
+            opp = self._hill[oppid]
+            if oppid < newid:
+                self._results[opp][newprog] = [-p for p in oppres]
+            elif newid < oppid:
+                self._results[newprog][opp] = oppres
+
+        # recompute points and ranking
+
+        self._rescore()
+
+        # update source repository
+
+        newp['file'] = self._save(replaced, newprog, code)
+
+        # return summary information about the new program
+
+        return '{0}: points {1:.2f}, score {2:.2f}/{3}, rank {4:d}/{5:d}'.format(
+            newprog,
+            newp['points'],
+            newp['score'], self._max_score,
+            1+newp['rank'], len(self._progs))
+
+    # point and score computations
+
+    def _rescore(self):
+        n = len(self._progs)
+
+        # recompute points for all programs
+
+        points = [0] * n
+
+        for idA, progA, idB, progB in self.idpairs():
+            s = sum(self._results[progA][progB])
+            points[idA] += s
+            points[idB] -= s
+
+        for i, p in enumerate(points):
+            self._progs[self._hill[i]]['points'] = p / (2*self._tapecount)
+
+        # recompute scores
+
+        scores = self._scoring['score'](self)
+        for prog, p in self._progs.items():
+            p['score'] = scores[prog]
+
+        # recompute ranking
+
+        self._ranking = sorted(self._progs.keys(),
+                               key=lambda prog: self._progs[prog]['score'],
+                               reverse=True)
+        for r, prog in enumerate(self._ranking):
+            self._progs[prog]['rank'] = r
+
+    # reports
+
+    def write_report(self, out):
+        n = len(self._progs)
+
+        def sym(r):
+            if r < 0: return '-'
+            if r > 0: return '+'
+            return '0'
+
+        out.write('Pos/ID   Score  Points  Program\n')
+        for rank, (prog, points, score) in enumerate(self.ranking()):
+            out.write('{0:6d} {1:7.2f} {2:7.2f}  {3}\n'.format(rank+1, score, points, prog))
+        out.write('\n')
+
+        out.write('   | ' +
+                  ' '.join(str(i//10) if i//10 > 0 else ' ' for i in range(1,n+1)) +
+                  ' |\n')
+        out.write('   | ' +
+                  ' '.join(str(i%10) for i in range(1,n+1)) +
+                  ' | score | pts   | program\n')
+        for rank, (prog, points, score) in enumerate(self.ranking()):
+            out.write('{0:2d} | '.format(rank+1) +
+                      ' '.join(sym(sum(self.results(prog, opp))) if opp != prog else ' '
+                               for opp, _, _ in self.ranking()) +
+                      ' |{0:6.1f} |{1:6.1f} | {2}\n'.format(score, points, prog))
+
+    def write_breakdown(self, prog, out):
+        sym = { -1: '>', 0: 'X', +1: '<' }
+        t = self._tapecount
+
+        for opp in sorted(self._hill):
+            if opp == prog:
+                continue
+
+            res = self.results(prog, opp)
+            tot = sum(res)
+
+            out.write('{0} vs {1}\n'.format(prog, opp))
+            out.write(''.join(sym[r] for r in res[:t]) +
+                      ' ' +
+                      ''.join(sym[r] for r in res[t:]) +
+                      ' {0:d}\n'.format(tot))
+            if tot == 0:
+                out.write('Tie.\n')
+            else:
+                out.write('{0} wins.\n'.format(prog if tot > 0 else opp))
+            out.write('\n')
+
+# git helper
+
+def git(*args):
+    sp.check_call(('git',) + args)
---
+++ zhill/score.py
@@ -0,0 +1,140 @@
+import numpy as np
+
+# "points" method: sum of raw duel points, moved to range [0, 2*(N-1)]
+
+def points_score(hill):
+    n = len(hill)
+    return dict((p, hill.points(p) + (n-1)) for p in hill)
+
+def points_max(hill):
+    return '{0:d}'.format(2 * (len(hill) - 1))
+
+# "trad" method: conventional egojoust scoring
+
+# N = number of programs, M = N-1, K = maximum duel points (2*tapecount)
+# worth of p = (points of p + N) / (M*2)
+# base score of p:
+#   for each program i != p:
+#     if p's duel points against i > 0:
+#       p's base score += i's worth * p-vs-i duel points / K
+# score of p = (base * 200) / M
+
+def trad_score(hill):
+    n = len(hill)
+    k = 2 * hill.tapecount()
+
+    worth = dict((p, (hill.points(p) + n) / (2 * (n-1))) for p in hill)
+
+    base = dict((p, 0) for p in hill)
+    for progA, progB in hill.pairs():
+        dp = sum(hill.results(progA, progB))
+        if dp > 0:
+            base[progA] += worth[progB] * dp / k
+        elif dp < 0:
+            base[progB] += worth[progA] * (-dp) / k
+
+    return dict((p, 200 * b / (n-1)) for p, b in base.items())
+
+# "iterative" method: fixed point of a trad-inspired scoring scheme
+
+# score^(0) = average of raw duel points, scaled to [0,1] possible range
+# score^(i):
+#   ... preferrably something that preserves sum(score) ...
+#   for program p:
+#     for each opponent o != p:
+#       ...
+
+def iter_score(hill):
+    n = len(hill)
+    k = 2 * hill.tapecount()
+
+    # pre compute "positive scaled i-vs-j duel points" matrix
+    dp = np.zeros((n,n))
+    for idA, progA, idB, progB in hill.idpairs():
+        t = sum(hill.results(progA, progB))
+        if t > 0:
+            dp[idA,idB] = t / k
+        elif t < 0:
+            dp[idB,idA] = (-t) / k
+
+    # initialize base score from raw points
+    s = np.zeros((n,1))
+    for i, prog in enumerate(hill):
+        s[i,0] = (hill.points(prog) + (n-1)) / (2 * (n-1))
+
+    # iterate until convergence, or for maximum number of steps
+    for step in range(100):
+        ps = s
+        s = np.dot(dp, ps)
+        s = s / s.sum() * (n/2)
+        if abs(s - ps).mean() < 0.000001:
+            break
+
+    return dict((p, 100*s[i,0]) for i, p in enumerate(hill))
+
+# "markov" method: stationary distribution of a Markov chain
+
+# let there be a (first-order) Markov chain, with the transition
+# probabilities defined as
+#
+#  t(a,b) = |{losses of a against b}| / (N*K)
+#  t(a,a) = 1 - sum_{b!=a} t(a,b)
+#
+# let score be the stationary distribution. what happens?
+#
+# - an entirely losing program will have a score of 0: good
+# - an entirely winning program may have a small score: bad
+#
+# simple fix: normalize to [0, 100] by scaling with max score
+
+def markov_score(hill):
+    n = len(hill)
+    k = 2 * hill.tapecount()
+
+    t = np.zeros((n,n))
+    for idA, progA, idB, progB in hill.idpairs():
+        results = hill.results(progA, progB)
+        lossAB = sum(1 for r in results if r == -1)
+        lossBA = sum(1 for r in results if r == 1)
+        t[idA,idB] = lossAB / (n * k)
+        t[idB,idA] = lossBA / (n * k)
+
+    for i in range(n):
+        t[i,i] = 1 - t[i,:].sum()
+
+    # speed up converging by squaring t a couple of times
+    for step in range(5):
+        t = np.dot(t, t)
+    # converge
+    d = np.ones((1,n)) / n
+    for step in range(100): # maximum number of steps
+        pd = d
+        d = np.dot(d, t)
+        if abs(d-pd).mean() < 0.000001:
+            break
+
+    d /= d.max()
+    d *= 100
+
+    return dict((p, d[0,i]) for i, p in enumerate(hill))
+
+# all known scoring methods
+
+scorings = {
+    'points': {
+        'score': points_score,
+        'max': points_max
+    },
+    'trad': {
+        'score': trad_score,
+        'max': lambda h: '100'
+    },
+    'iter': {
+        'score': iter_score,
+        'max': lambda h: '100'
+    },
+    'markov': {
+        'score': markov_score,
+        'max': lambda h: '100'
+    }
+}
