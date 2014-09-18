4b9ddf960fe6e8ff9813a7f617a7e96af3950705
Heikki Kallasjoki <fis@zem.fi>
2014-09-18 22:28:09 +0300

    Handle HTTP requests submitting code

--- zhill/bot.py
+++ zhill/bot.py
@@ -1,5 +1,7 @@
 import re
 import traceback
+import urllib.error
+import urllib.request
 from datetime import datetime
 
 import irc.bot
@@ -23,12 +25,17 @@ class Bot(irc.bot.SingleServerIRCBot):
 
     def on_pubmsg(self, c, e):
         msg = e.arguments[0].split(' ', 3)
+        if len(msg) > 0 and msg[0] == self._command:
+            self._handle(c, e.target, e.source.nick + ': ', e.source.nick, msg)
 
-        if len(msg) == 0 or msg[0] != self._command:
-            return
+    def on_privmsg(self, c, e):
+        msg = e.arguments[0].split(' ', 3)
+        if len(msg) > 0 and msg[0] == self._command:
+            self._handle(c, e.source.nick, '', e.source.nick, msg)
 
+    def _handle(self, c, target, prefix, nick, msg):
         if len(msg) <= 2:
-            c.privmsg(e.target, e.source.nick + ': "!bfjoust progname code". See http://zem.fi/bfjoust/ for documentation.')
+            c.privmsg(target, prefix + '"!bfjoust progname code". See http://zem.fi/bfjoust/ for documentation.')
             return
 
         orig_prog = msg[1]
@@ -36,18 +43,20 @@ class Bot(irc.bot.SingleServerIRCBot):
 
         prog = re.sub(r'[^a-zA-Z0-9_-]', '', orig_prog)
         if len(prog) < 0.75*len(orig_prog):
-            c.privmsg(e.target, e.source.nick + ': Program name looks like gibberish. Did you forget it?')
+            c.privmsg(target, prefix + 'Program name looks like gibberish. Did you forget it?')
             return
 
-        prog = e.source.nick + '~' + prog
+        prog = nick + '~' + prog
 
         tstamp = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-        self._log.write('{0} {1} {2} {3}\n'.format(tstamp, e.source.nick, prog, code))
+        self._log.write('{0} {1} {2}\n'.format(tstamp, nick, code))
+        self._log.flush()
 
         if re.match(r'(?:https?|ftp)://', code):
-            # TODO: fetch code over HTTP if required
-            c.privmsg(e.target, e.source.nick + ': URL fetch not done yet SORRY')
-            return
+            err, code = fetch(code)
+            if err is not None:
+                c.privmsg(target, prefix + 'URL fetch problems: ' + err)
+                return
         else:
             code = code.encode('utf-8')
 
@@ -56,9 +65,9 @@ class Bot(irc.bot.SingleServerIRCBot):
 
             err, result = self._hill.challenge(prog, code)
             if err is not None:
-                c.privmsg(e.target, err)
+                c.privmsg(target, err)
                 return
-            c.privmsg(e.target, result)
+            c.privmsg(target, result)
 
             # update web components
 
@@ -74,5 +83,19 @@ class Bot(irc.bot.SingleServerIRCBot):
 
         except Exception:
             traceback.print_exc(file=self._log)
-            c.privmsg(e.target, 'I broke down! Ask fizzie to help! The details are in the log!')
+            c.privmsg(target, 'I broke down! Ask fizzie to help! The details are in the log!')
             self.die()
+
+def fetch(url):
+    MAXSIZE = 2*1024*1024
+
+    try:
+        with urllib.request.urlopen(url, timeout=10) as resp:
+            if resp.getcode() != 200:
+                return 'request failed: ' + resp.getcode(), None
+            code = resp.read(MAXSIZE+1)
+            if len(code) > MAXSIZE:
+                return 'maximum size limit ({0} bytes) exceeded', None
+            return None, code
+    except urllib.error.URLError as e:
+        return e.reason, None
